2602f2badbcb6f2ad6b3a4204eaeffd7330c595f
SergeyMaximov <mrknitter7@gmail.com>
2025-02-28 18:23:00 +0300

    list-filtering.js revised

--- 7kyu/list-filtering.js
+++ 7kyu/list-filtering.js
@@ -5,10 +5,7 @@
 // filter_list([1,'a','b',0,15]) == [1,0,15]
 // filter_list([1,2,'aasf','1','123',123]) == [1,2,123]
 
-function filter_list(l) {
-    const filteredArray = [];
-    l.filter(elem => {
-        if (typeof elem === 'number') filteredArray.push(elem);
-    })
-    return filteredArray;
-}
+const filter_list = (l) => l.filter(element => typeof element === 'number')
+
+
+console.log(filter_list([1,2,'a',5, 'fuel', 'b'])) //[1,2,5]
