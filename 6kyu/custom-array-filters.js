01c16550ef2681fa4120c980e03c75bc5557b650
SergeyMaximov <mrknitter7@gmail.com>
2025-03-02 15:00:55 +0300

    custom-array-filters.js

--- 6kyu/custom-array-filters.js
+++ 6kyu/custom-array-filters.js
@@ -44,13 +44,3 @@ Array.prototype.inRange = function (min, max) {
     });
 };
 
-
-
-console.log([1,2,3,4,5].even());
-console.log([1,2,3,4,5].odd());
-console.log([1,2,3,4,5].under(4));
-console.log([1,2,3,4,5].over(4));
-console.log([1,2,3,4,5].inRange(1, 3));
-console.log([1,2,18,19,22,20,21,30,40,50,100].even().inRange(18,30));
-console.log(["a", 1, "b", 300, "x", "q", 63, 122, 181, "z", 0.83, 0.12].even());
-// console.log(["a", 1, "b", 300, "x", "q", 63, 122, 181, "z", 0.83, 0.12].string());
